9c669b8f7b6003e0a4c5f6b6549ee890e7fdbb13
Edvinas <edvinasalimas98@gmail.com>
2022-05-22 14:41:53 +0300

    Authentication guard update

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   ActivatedRouteSnapshot, CanActivate,
   Router,
   RouterStateSnapshot,
   UrlTree
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import {UserService} from "../services/user.service";
 
 @Injectable({
@@ -21,7 +21,8 @@ export class AuthenticationGuard implements CanActivate {
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const token = localStorage.getItem('token');
-    if (token !== null && this.userService.validate(token) !== null) {
+
+    if (token !== null && token !== 'undefined' && this.userService.validate(token) !== null) {
       return true;
     }
     this.router.navigate(['/login']);
