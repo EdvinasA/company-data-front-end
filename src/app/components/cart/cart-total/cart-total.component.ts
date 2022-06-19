c3271cc3e0ae749b5b8b0a15f2cbf75f1a18749f
Edvinas <edvinasalimas98@gmail.com>
2022-06-19 17:19:37 +0300

    SHOP-24 Finish visual part of the cart

--- src/app/components/cart/cart-total/cart-total.component.ts
+++ src/app/components/cart/cart-total/cart-total.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart-total',
   templateUrl: './cart-total.component.html',
-  styleUrls: ['./cart-total.component.css']
+  styleUrls: ['./cart-total.component.scss']
 })
 export class CartTotalComponent implements OnInit {
 
