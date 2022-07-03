a3adb256f152aa630b12c8db6d86c765a1ee33f6
Edvinas <edvinasalimas98@gmail.com>
2022-07-03 08:15:23 +0300

    SHOP-35 Fix imports

--- src/app/components/cart/cart.module.ts
+++ src/app/components/cart/cart.module.ts
@@ -3,18 +3,18 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
 import {BrowserModule} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
-
-import {CartDeliveryComponent} from "./cart-delivery/cart-delivery.component";
-import {CartTotalComponent} from "./cart-total/cart-total.component";
-import {CartPopoverComponent} from "./cart-popover/cart-popover.component";
-import {CartComponent} from "./cart.component";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {PaginatePipe} from "ngx-pagination";
 import {DecimalPipe} from "@angular/common";
 import {MdePopoverModule} from "@material-extended/mde";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+
+import {PaginatePipe} from "ngx-pagination";
+
+import {CartDeliveryComponent} from "./cart-delivery/cart-delivery.component";
+import {CartTotalComponent} from "./cart-total/cart-total.component";
+import {CartComponent} from "./cart.component";
 
 @NgModule({
   declarations: [
--- src/app/components/products-list/products-list.module.ts
+++ src/app/components/products-list/products-list.module.ts
@@ -1,10 +1,6 @@
 import {NgModule} from "@angular/core";
-import {ProductsListAsCardComponent} from "./products-list-as-card/products-list-as-card.component";
-import {ProductsListItemsComponent} from "./products-list-items/products-list-items.component";
-import {ProductsListComponent} from "./products-list.component";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {MatIconModule} from "@angular/material/icon";
-import {NgxPaginationModule, PaginatePipe} from "ngx-pagination";
 import {BrowserModule} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
@@ -12,11 +8,17 @@ import {MatDividerModule} from "@angular/material/divider";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatSelectModule} from "@angular/material/select";
 import {MatOptionModule} from "@angular/material/core";
-import {ProductToCartDialogComponent} from "./product-to-cart-dialog/product-to-cart-dialog.component";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
 import {MatMenuModule} from "@angular/material/menu";
 import {DecimalPipe} from "@angular/common";
+
+import {NgxPaginationModule, PaginatePipe} from "ngx-pagination";
+
+import {ProductsListAsCardComponent} from "./products-list-as-card/products-list-as-card.component";
+import {ProductsListItemsComponent} from "./products-list-items/products-list-items.component";
+import {ProductsListComponent} from "./products-list.component";
+import {ProductToCartDialogComponent} from "./product-to-cart-dialog/product-to-cart-dialog.component";
 
 @NgModule({
   declarations: [
