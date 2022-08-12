f0391ef9ade353003bcbca0c615cae77800a09ca
Edvinas <edvinasalimas98@gmail.com>
2022-08-12 20:50:57 +0300

    SHOP-43 Add headers and additional information

--- src/app/components/cart/cart-checkout-information/cart-checkout-information.component.ts
+++ src/app/components/cart/cart-checkout-information/cart-checkout-information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-cart-checkout-information',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartCheckoutInformationComponent implements OnInit {
 
+  shippingDeliveryForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    postalCode: new FormControl('', [Validators.required]),
+    additionalInformation: new FormControl('', ),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  formControl(input: string) {
+    return this.shippingDeliveryForm.get(input);
   }
 
 }
