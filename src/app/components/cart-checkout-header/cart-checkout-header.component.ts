a19d791bc91569d957a41959f471b2888da89572
Edvinas Alimas <edvinas.alimas@devbridge.com>
2022-08-23 08:33:32 +0300

    SHOP-75 Update

--- src/app/components/cart-checkout-header/cart-checkout-header.component.ts
+++ src/app/components/cart-checkout-header/cart-checkout-header.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./cart-checkout-header.component.scss'],
 })
 export class CartCheckoutHeaderComponent implements OnInit {
-  @Input() title: string = 'a';
+  @Input() title: string = 'ASdGWASC';
 
   constructor() {}
 
