b426682f3a6630643fe3ea2c5deaabeed7fccee5
Edvinas <edvinasalimas98@gmail.com>
2022-09-07 06:40:20 +0300

    SHOP-96 Add selectable year and months

--- src/app/components/cart/checkout/checkout-credit-cart/checkout-credit-cart.component.ts
+++ src/app/components/cart/checkout/checkout-credit-cart/checkout-credit-cart.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {Country} from "../../../../models/country";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {CheckoutService} from "../../../../services/checkout.service";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Country } from '../../../../models/country';
+import { CheckoutService } from '../../../../services/checkout.service';
 
 @Component({
   selector: 'app-checkout-credit-cart',
   templateUrl: './checkout-credit-cart.component.html',
-  styleUrls: ['./checkout-credit-cart.component.scss']
+  styleUrls: ['./checkout-credit-cart.component.scss'],
 })
 export class CheckoutCreditCartComponent implements OnInit {
-  countries: Country[] = [];
-  checkoutCreditCardForm = new FormGroup({
+  public listOfYears: number[] = [
+    2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033,
+    2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045,
+    2046, 2047, 2048, 2049, 2050,
+  ];
+  public listOfMonths: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  public countries: Country[] = [];
+  public checkoutCreditCardForm = new FormGroup({
     cardNumber: new FormControl('', Validators.required),
     expiration: new FormControl('', Validators.required),
     cvc: new FormControl('', Validators.required),
