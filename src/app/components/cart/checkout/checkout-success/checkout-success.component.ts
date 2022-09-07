76a559c28f120ba751df54ffca928a670a01520d
Edvinas <edvinasalimas98@gmail.com>
2022-09-07 05:33:56 +0300

    SHOP-95 Update failed and success pages

--- src/app/components/cart/checkout/checkout-success/checkout-success.component.ts
+++ src/app/components/cart/checkout/checkout-success/checkout-success.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-success',
   templateUrl: './checkout-success.component.html',
-  styleUrls: ['./checkout-success.component.scss']
+  styleUrls: ['./checkout-success.component.scss'],
 })
 export class CheckoutSuccessComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToHome() {
+    this.router.navigate(['/']);
   }
-
 }
