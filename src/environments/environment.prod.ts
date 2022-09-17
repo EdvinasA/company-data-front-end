1e59523828aaad25ad9c756beefd23d6b8d72bfe
Edvinas <edvinasalimas98@gmail.com>
2022-09-17 22:03:49 +0300

    SHOP-102 Update environemnt.prod.ts and angular.json files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  apiUrl: 'http://localhost:8081/api',
+  stripeKey:
+    'pk_test_51LPrdZK7iwO35NMSXbb2jpzfzMDkMinfb05aXEzXpQlw9SamixH4fvn496XJnZWJwDGw4OpHiWSO59ovulPECQW200WUZ7QXZg',
 };
